8ce8e2480ca5306edaa7c1f267de5417d70614d9
Electro18 <74699991+Electro18@users.noreply.github.com>
2022-05-20 20:12:46 +0400

    .

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import discord
 from discord.ext import commands
 import akinator
 import os
-import json
 
 
 intents = discord.Intents.all()
@@ -16,34 +15,13 @@ async def on_ready():
   print('------')
 
 
-def create_setting_u(ctx):
-  with open("config/users.json", "r") as f:
-    data = json.load(f)
-
-  data[str(ctx.author.id)] = {}
-  data[str(ctx.author.id)]["child"] = True
-  data[str(ctx.author.id)]["lang"] = "en"
-
-  with open("config/users.json", "w") as f:
-    json.dump(data, f)
-
 @client.command()
 async def start(ctx):
-  with open("config/users.json", "r") as f:
-    data = json.load(f)
-  if str(ctx.author.id) not in data:
-    create_setting_u(ctx=ctx)
-  with open("config/users.json", "r") as f:
-    data = json.load(f)
-  
-  child = data[str(ctx.author.id)]["child"]
-  lang = data[str(ctx.author.id)]["lang"]
 
   def check(m):
     return m.author.id == ctx.author.id and m.channel.id == ctx.channel.id
 
-  q = aki.start_game(language=lang, child_mode=child)
-  await ctx.send(lang + "i")
+  q = aki.start_game()
 
   while aki.progression <= 80:
     em=discord.Embed(title=q, description=f"Progress: {aki.progression}\n\nStep: {aki.step}", color=discord.Color.random())
@@ -62,7 +40,10 @@ async def start(ctx):
 
   aki.win()
 
-  await ctx.send(f"It's {aki.first_guess['name']} ({aki.first_guess['description']})! Was I correct?\n{aki.first_guess['absolute_picture_path']}\n\t")
+  em = discord.Embed(title=aki.first_guess["name"], description=f"Description: " + aki.first_guess["description"] + "\n\nRanking: " + aki.first_guess["ranking"], color = discord.Color.random())
+  em.set_footer(text="Am I correct? Respond with yes or no.", icon_url=client.user.avatar_url)
+  em.set_image(url=aki.first_guess["absolute_picture_path"])
+  await ctx.send(embed=em)
 
   correct = message = await client.wait_for("message", check=check)
   a = message.content
@@ -72,28 +53,6 @@ async def start(ctx):
   else:
     await ctx.send("Oof\n")
 
-
-
-@client.command()
-async def language(ctx, lang=None):
-  if lang == None:
-    await ctx.send("Please state 1 of the following languages: `en`,`ar`,`cn`,`de`,`es`,`fr`,`il`,`it`,`jp`,`kr`,`pl`,`nl`,`pt`,`ru`,`tr`,`id`.")
-  else:
-
-    with open("config/users.json", "r") as f:
-      data = json.load(f)
-    if str(ctx.author.id) not in data:
-      create_setting_u(ctx=ctx)
-  
-    with open("config/users.json", "r") as f:
-      data = json.load(f)
-  
-    data[str(ctx.author.id)]["lang"] = lang
-  
-    with open("config/users.json", "w") as f:
-      json.dump(data, f)
-      
-    await ctx.send(f"Set lang to {lang}!")
 
 
 TOKEN=os.getenv("BOT_TOKEN")
