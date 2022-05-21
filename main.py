2cb21af4f32c3cfdc5104d7bd98823bdbf9097f1
Electro18 <74699991+Electro18@users.noreply.github.com>
2022-05-21 21:42:48 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import discord
 from discord.ext import commands
 import akinator
 import os
+import asyncio
 
 
 intents = discord.Intents.all()
@@ -18,47 +19,79 @@ async def on_ready():
 
 @client.command()
 async def start(ctx, language="en", child_mode=True):
+  async with ctx.typing():
+    await asyncio.sleep(0)
 
   def check(m):
     return m.author.id == ctx.author.id and m.channel.id == ctx.channel.id
 
-  q = aki.start_game(language,child_mode)
+  q = await aki.start_game(language,child_mode)
+  premessage = ctx.message
+  playagain = True
 
-  while aki.progression <= 85:
-    em=discord.Embed(title=f"Question {aki.step + 1}", description=f"**{q}**\n[yes (**y**) / no (**n**) / idk (**i**) / probably (**p**) / probably not (**pn**)]\n[back (**b**) / stop (**s**)]", color=discord.Color.from_rgb(255,245,0))
-    em.set_footer(text="Respond in 120 seconds!", icon_url=client.user.avatar.url)
-    await ctx.message.reply(embed=em)
-
-    message = message = await client.wait_for("message", check=check, timeout=120)
-    a = message.content
-
-    if a.lower() == "back" or a.lower() == "b":
-      try:
-        q = aki.back()
-      except akinator.CantGoBackAnyFurther:
-        await ctx.send("Cannot go back!")
+  while playagain == True:
+    while aki.progression <= 85:
+      async with ctx.typing():
+        await asyncio.sleep(0)
+      em=discord.Embed(title=f"Question {aki.step + 1}", description=f"**{q}**\n[yes (**y**) / no (**n**) / idk (**i**) / probably (**p**) / probably not (**pn**)]\n[back (**b**) / stop (**s**)]", color=discord.Color.from_rgb(255,245,0))
+      await premessage.reply(embed=em)
+  
+      message = message = await client.wait_for("message", check=check, timeout=5)
+      premessage = message
+      a = message.content
+  
+      if a.lower() == "back" or a.lower() == "b":
+        try:
+          q = await aki.back()
+        except akinator.CantGoBackAnyFurther:
+          await ctx.send("Go back to where? This is your first question. :face_with_monocle:")
+          pass
+      elif a.lower() == "stop" or a.lower() == "s":
+        await aki.close()
+        await ctx.message.reply("Stoped the game! =(")
         pass
-    elif a.lower() == "stop" or a.lower() == "s":
-      await ctx.message.reply("Currently making this feautre so.... Sorry?")
-      pass
+      else:
+        q = await aki.answer(a)
+  
+    async with ctx.typing():
+      await asyncio.sleep(0)
+    aki.win()
+  
+    em = discord.Embed(title=aki.first_guess["name"], description=aki.first_guess["description"] + "\nRanked: **#" + aki.first_guess["ranking"] + "**\n\n[yes (**y**) / no (**b**)]\n[back (**b**)]", color=discord.Color.from_rgb(255,245,0))
+    em.set_image(url=aki.first_guess["absolute_picture_path"])
+    em.set_author(text="Is this your charecter?")
+    await ctx.send(embed=em)
+  
+    correct = message = await client.wait_for("message", check=check)
+    premessage = correct
+    a = correct.content
+  
+    async with ctx.typing():
+      await asyncio.sleep(0)
+
+    if a.lower() == "yes" or a.lower() == "y":
+      em = discord.Embed(title="Yay, Got another one right!", description="Want to one more time? :upside_down:\n\n[yes (**y**) / no (**n**)", color=discord.Color.from_rgb(255,245,0))
+      await premessage.reply(embed=em)
+      another = message = await client.wait_for("message", check=check)
+      a = another.content
+      premessage = another
+      if a.lower == "y" or a.lower() == "yes":
+        playagain = True
+      else:
+        playagain = False
+        await another.reply("Ok =(")
     else:
-      q = aki.answer(a)
-        
-
-  aki.win()
-
-  em = discord.Embed(title=aki.first_guess["name"], description=f"Description: " + aki.first_guess["description"] + "\n\nRanking: " + aki.first_guess["ranking"], color = discord.Color.random())
-  em.set_footer(text="Am I correct? Respond with yes or no.", icon_url=client.user.avatar.url)
-  em.set_image(url=aki.first_guess["absolute_picture_path"])
-  await ctx.send(embed=em)
-
-  correct = message = await client.wait_for("message", check=check)
-  a = correct.content
-
-  if a.lower() == "yes" or a.lower() == "y":
-    await ctx.send("Yay\n")
-  else:
-    await ctx.send("Oof\n")
+      em = discord.Embed(title="Oof, Got one wrong... :disappointed:", description="Want me to try one more time? :upside_down:\n\n[yes (**y**) / no (**n**)", color=discord.Color.from_rgb(255,245,0))
+      await premessage.reply(embed=em)
+      another = message = await client.wait_for("message", check=check)
+      a = another.content
+      premessage = another
+      if a.lower == "y" or a.lower() == "yes":
+        playagain = True
+      else:
+        playagain = False
+        await another.reply("Ok =(")
+    await aki.close()
 
 
 
